365cf3151cf595a6017739620a9d8337ebd82b52
alessandrofrancescostellato <unabirraodue@gmail.com>
2023-09-13 12:40:16 +0200

    efi

---
+++ src/main/java/com/example/test2/Product.java
@@ -0,0 +1,12 @@
+package com.example.test2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    String name;
+}
